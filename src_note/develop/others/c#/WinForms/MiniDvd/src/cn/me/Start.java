5ec7dd5ed19a90d408b42d18377b4067377d7b12
ljca <k_y_z_s@yeah.net>
2022-10-26 22:17:04 +0800

    update

--- src_note/develop/others/c#/WinForms/MiniDvd/src/cn/me/Start.java
+++
@@ -1,8 +0,0 @@
-package cn.me;
-public class Start {
-	public static void main(String[] args) {
-		//DvdMgr.CreateWindow();
-		DvdSet.returnMenu();
-	}
-
-}
--- src_note/develop/others/c#/WinForms/Super/SuperMark/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-use [SuperMark]
-go
-select [CustomerId],[CustomerType],[Score],[StatesName] from [UserInfo] as p,[States] as c
-where c.Id = p.StatesId
-go
